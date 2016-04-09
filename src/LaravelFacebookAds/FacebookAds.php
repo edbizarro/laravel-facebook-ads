fe2b31ed7981594cdc9e1507c926caca982700c7
Eduardo Bizarro <edbizarro@gmail.com>
2016-04-08 22:42:56 -0300

    iniciada implementação de Facebook auth

--- src/LaravelFacebookAds/FacebookAds.php
+++ src/LaravelFacebookAds/FacebookAds.php
@@ -3,10 +3,40 @@
 namespace LaravelFacebookAds;
 
 use FacebookAds\Api;
+use Facebook\Facebook;
 
 /**
  * Class FacebookAds.
  */
 class FacebookAds
 {
+    /**
+     * @var Facebook
+     */
+    protected $fbClient;
+
+    /**
+     * FacebookConnect constructor.
+     *
+     * @param array $config
+     */
+    public function __construct(array $config = [])
+    {
+        $config = array_merge([
+            'app_id' => '',
+            'app_secret' => '',
+            'default_graph_version' => Facebook::DEFAULT_GRAPH_VERSION,
+            'enable_beta_mode' => false,
+            'http_client_handler' => null,
+            'persistent_data_handler' => null,
+            'pseudo_random_string_generator' => null,
+            'url_detection_handler' => null,
+        ], $config);
+
+        if (session_status() == PHP_SESSION_NONE) {
+            session_start();
+        }
+
+        $this->fbClient = new Facebook($config);
+    }
 }
