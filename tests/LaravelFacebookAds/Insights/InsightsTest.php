5e54165a653c39379e7060f14ebbd02ad44c1814
Eduardo Bizarro <edbizarro@gmail.com>
2018-09-11 14:14:35 -0300

    remove tests

--- tests/LaravelFacebookAds/Insights/InsightsTest.php
+++ tests/LaravelFacebookAds/Insights/InsightsTest.php
@@ -2,11 +2,13 @@
 
 namespace LaravelFacebookAds\Tests\Insights;
 
+use Edbizarro\LaravelFacebookAds\FacebookAds;
 use LaravelFacebookAds\Tests\BaseTest;
 
 class InsightsTest extends BaseTest
 {
     public function test_ad_insight()
     {
+
     }
 }
