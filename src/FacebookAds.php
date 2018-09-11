39ded47d0ba2ae1eb538f3f41a7a06421c5f5e3b
Eduardo Bizarro <edbizarro@gmail.com>
2018-09-11 14:15:04 -0300

    ads

--- src/FacebookAds.php
+++ src/FacebookAds.php
@@ -4,9 +4,35 @@ namespace Edbizarro\LaravelFacebookAds;
 
 use Edbizarro\LaravelFacebookAds\Entities\Campaigns;
 use Edbizarro\LaravelFacebookAds\Entities\AdAccounts;
+use Illuminate\Support\Traits\Macroable;
 
 class FacebookAds extends AbstractFacebookAds
 {
+    use Macroable;
+
+
+    /**
+     * @param Period $period
+     * @param $accountId
+     * @param string $level {ad, adset, campaign, account}
+     * @param array $params
+     *
+     * @see https://developers.facebook.com/docs/marketing-api/insights
+     */
+    public function insights(
+        Period $period,
+        $accountId,
+        $level,
+        array $params
+    ) {
+        return (new Insights)->get(
+            $period,
+            $accountId,
+            $level,
+            $params
+        );
+    }
+
     /**
      * @return AdAccounts
      */
