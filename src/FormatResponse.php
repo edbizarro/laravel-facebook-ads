2446310670fec1394dd4d05dfccf1a4883fa58ed
Eduardo Bizarro <edbizarro@gmail.com>
2018-09-11 14:15:04 -0300

    ads

---
+++ src/FormatResponse.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Edbizarro\LaravelFacebookAds;
+
+
+use FacebookAds\Cursor;
+use FacebookAds\Object\AbstractObject;
+use Illuminate\Support\Collection;
+
+trait FormatResponse
+{
+    protected function format(Cursor $response)
+    {
+        $data = null;
+
+//        dd($response->getArrayCopy());
+        try {
+            switch (true) {
+                case $response instanceof Cursor:
+                    $data = new Collection;
+
+                    foreach ($response->getLastResponse()->getContent() as $item) {
+                        $data->push($item);
+                    }
+                    break;
+                case $response instanceof AbstractObject:
+                    $data = $response->getLastResponse()->getContent();
+                    break;
+            }
+
+            return $data;
+        } catch (Exception $e) {
+            return collect();
+        }
+    }
+}
