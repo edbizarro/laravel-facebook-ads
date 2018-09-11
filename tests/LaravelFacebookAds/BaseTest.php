2508e594133208970cea0c9e80b1cb8d8f25f997
Eduardo Bizarro <edbizarro@gmail.com>
2018-09-11 12:25:31 -0300

    tests

--- tests/LaravelFacebookAds/BaseTest.php
+++ tests/LaravelFacebookAds/BaseTest.php
@@ -2,6 +2,8 @@
 
 namespace LaravelFacebookAds\Tests;
 
+use Edbizarro\LaravelFacebookAds\Facades\FacebookAds;
+use Edbizarro\LaravelFacebookAds\Providers\LaravelFacebookServiceProvider;
 use Orchestra\Testbench\TestCase;
 use Mockery as m;
 
@@ -19,5 +21,12 @@ abstract class BaseTest extends TestCase
     public function setUp()
     {
         parent::setUp();
+
+        FacebookAds::init(env('FB_ACCESS_TOKEN'));
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [LaravelFacebookServiceProvider::class];
     }
 }
---
+++ tests/LaravelFacebookAds/Insights/InsightsTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace LaravelFacebookAds\Tests\Insights;
+
+use LaravelFacebookAds\Tests\BaseTest;
+
+class InsightsTest extends BaseTest
+{
+    public function test_ad_insight()
+    {
+    }
+}
