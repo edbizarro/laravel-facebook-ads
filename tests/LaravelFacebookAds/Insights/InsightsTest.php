240aa1eaa96c869caa1c42ecb53f374367aa4e11
Eduardo Bizarro <edbizarro@gmail.com>
2018-09-11 14:17:03 -0300

    style

--- tests/LaravelFacebookAds/Insights/InsightsTest.php
+++ tests/LaravelFacebookAds/Insights/InsightsTest.php
@@ -2,13 +2,11 @@
 
 namespace LaravelFacebookAds\Tests\Insights;
 
-use Edbizarro\LaravelFacebookAds\FacebookAds;
 use LaravelFacebookAds\Tests\BaseTest;
 
 class InsightsTest extends BaseTest
 {
     public function test_ad_insight()
     {
-
     }
 }
