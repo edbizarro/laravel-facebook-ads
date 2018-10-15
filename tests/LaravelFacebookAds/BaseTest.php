dd844e45c11aea0b70c3e419ac964a7ba1e90eff
Eduardo Bizarro <edbizarro@gmail.com>
2018-10-15 15:15:44 -0300

    code style

--- tests/LaravelFacebookAds/BaseTest.php
+++ tests/LaravelFacebookAds/BaseTest.php
@@ -2,13 +2,9 @@
 
 namespace LaravelFacebookAds\Tests;
 
-use Edbizarro\LaravelFacebookAds\Facades\FacebookAds;
 use Edbizarro\LaravelFacebookAds\Providers\LaravelFacebookServiceProvider;
-use FacebookAds\Logger\CurlLogger;
 use Orchestra\Testbench\TestCase;
 use Mockery as m;
-use Edbizarro\LaravelFacebookAds\Period;
-use FacebookAds\Object\Values\AdsInsightsLevelValues;
 
 /**
  * Class BaseTest.
